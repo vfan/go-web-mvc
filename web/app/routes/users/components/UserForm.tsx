fab9618ac2bbf3c1aec4f011b28bce81b11bbb43
vfan <haitao_wu@qq.com>
2025-05-13 23:45:28 +0800

    fix: 删除不存在的username字段，修复用户表单类型错误

--- web/app/routes/users/components/UserForm.tsx
+++ web/app/routes/users/components/UserForm.tsx
@@ -21,7 +21,6 @@ export default function UserForm({ user, onSuccess, onCancel }: UserFormProps) {
   useEffect(() => {
     if (user) {
       form.setFieldsValue({
-        username: user.username,
         email: user.email,
         role: user.role,
         password: '', // 编辑时不显示密码，但保留字段用于可选修改
@@ -62,16 +61,6 @@ export default function UserForm({ user, onSuccess, onCancel }: UserFormProps) {
       onFinish={handleSubmit}
     >
       {contextHolder}
-      <Form.Item
-        name="username"
-        label="用户名"
-        rules={[
-          { required: true, message: '请输入用户名' },
-          { min: 3, message: '用户名长度不能少于3个字符' }
-        ]}
-      >
-        <Input placeholder="请输入用户名" />
-      </Form.Item>
 
       <Form.Item
         name="email"
